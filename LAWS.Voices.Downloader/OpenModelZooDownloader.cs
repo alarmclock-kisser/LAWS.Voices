cf06a1b806683055c1886cbbe196114ccc0e23d7
Marcel King <marcel.king91299@gmail.com>
2026-05-29 21:29:51 +0200

    Projektdateien hinzufügen.

---
+++ LAWS.Voices.Downloader/OpenModelZooDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAWS.Voices.Downloader
+{
+    public static class OpenModelZooDownloader
+    {
+        public const string ModelsBaseUrl = "https://github.com/openvinotoolkit/open_model_zoo/tree/master/models";
+
+        public static Task<List<string>> GetAvailableModelsAsync()
+        {
+
+        }
+
+
+    }
+}
---
+++ LAWS.Voices.Downloader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LAWS.Voices.Downloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
